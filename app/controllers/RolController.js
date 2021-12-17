4376e11a82fd98b2034cad985894b1fe5347cc79
rompni <andresnavarrodev@gmail.com>
2021-12-17 03:29:22 -0500

    query buscar por nombre

--- app/controllers/RolController.js
+++ app/controllers/RolController.js
@@ -33,4 +33,23 @@ RolController.findAll = async (req, res) => {
     }
 };
 
+// TRAER ROL POR NOMBRE
+RolController.findByName = async (req, res) => {
+
+    try {
+        if (!req.body.name) {
+            res.status(400).send({message: "Debe proporcionar un nombre"})
+        }
+
+        RolModel.findOne({name: req.body.name}, (err, rol) => {
+            if (err) {
+                res.status(500).send({message: err});
+            }
+            res.status(200).json(rol);
+        });
+    } catch (err) {
+        res.status(500).send({message: err})
+    }
+}
+
 export default RolController;
